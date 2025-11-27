2e2436cf8883686b67e51e2177251c32d0e2219e
mpcapchka <143687336+mpcapchka@users.noreply.github.com>
2025-11-27 07:44:17 +0100

    Applying last changes to main branch from develop (#3)
    * Add dependency license information
    
    * window control design attempt;
    
    ---------
    
    Co-authored-by: Kostia Britikov <konstantin.britikov@gmail.com>

---
+++ src/SheduleHelper.WpfApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheduleHelper.WpfApp.ViewModel
+{
+    internal class MainWindowViewModel
+    {
+    }
+}
